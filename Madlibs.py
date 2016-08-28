1c411967e72c6c039478eecadd4777f0e3ed4a07
jmendez1340 <jasonmendez795@gmail.com>
2016-08-28 13:13:06 -0500

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,33 @@
+# Variables
+Time = "Tomorrow"
+Place = "Beach"
+Animal = "Penguin"
+Object = "Car"
+Action = "hug"
+Action2 = "hugging"
+Action3 = "Driving"
+Name = "Gaga"
+Thing = "Tires"
+Man = "Batman"
+Speed = "Sound"
+Law = "Jaywalking"
+
+
+# Don't Stop Me Now by Queen Madlibs By Jason Mendez
+print( Time + " I'm gonna have myself a real good time" + "," )
+print("I feel alive and the world I'll turn it inside out - yeah" + ",")
+print("And floating around in ecstasy" + ",")
+print("So don't " + Action + " me now Don't " + Action + " me " + ",")
+print("Cause I'm having a good time having a good time" + ",")
+
+print("I'm a shooting " + Object + " "+ Action3 + " through the " + Place + ",")
+print("Like a " + Animal + " defying the law of " + Law + "," )
+print("I'm a racing car passing by like Lady " + Name + ",")
+print("I'm gonna go go go" + ",")
+print("There's no " + Action2 + " me" + ",")
+
+print("I'm burning through the " + Thing + " yeah" + ",")
+print("Two hundred degrees" + ",")
+print("That's why they call me Mister " + Man + ",")
+print("I'm trav'ling at the speed of " + Speed + ",")
+print("I wanna make a supersonic " + Man + " out of you" + ",")
